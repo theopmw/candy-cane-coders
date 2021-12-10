287be4a40b52dea993403dea2bd99bbb7a41e6c8
Sean Murphy <mail.seanmurphy@gmail.com>
2021-12-10 21:52:18 +0000

    Added gift model

--- gifts/models.py
+++ gifts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Gift(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    image = models.ImageField(null=False, blank=False)
+    day = models.IntegerField(default=1, null=False, blank=False)
