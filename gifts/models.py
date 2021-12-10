c816874fd238a84138f98845b0843a7b98954f09
Sean Murphy <mail.seanmurphy@gmail.com>
2021-12-10 21:53:48 +0000

    Added gift model

--- gifts/models.py
+++ gifts/models.py
@@ -4,3 +4,6 @@ class Gift(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     image = models.ImageField(null=False, blank=False)
     day = models.IntegerField(default=1, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
