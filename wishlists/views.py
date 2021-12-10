6ab0d2373bb8ee94b86366b411bc8143e7421eef
Melinda Zhang <melinda_hongye@yahoo.com>
2021-12-10 23:39:18 +0000

    Edit wishlish template

--- wishlists/views.py
+++ wishlists/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def wishlists(request):
     context = {}
-    return render(request, "wishlists/wishlist.html", context)
+    template = "wishlists/wishlist.html"
+    return render(request, template, context)
+
+
+def add_to_wishlist(request):
+    return redirect("wishlists/")
+
+
+def remove_from_wishliast(request):
+    return redirect("wishlists/")
