124f7f7da307b1c01faaca3f07e68ee158ef8a8c
Luke Gibson <89612238+gibbo101@users.noreply.github.com>
2021-12-12 17:15:43 +0000

    Countdown.html and Admin nav bar (#31)
    * .
    
    * disable boxes once day = 11
    
    * set max day to 11
    
    * fix responsive nav bar
    
    * add admin dropdown
    
    * reposition admin button
    
    * add initial functionality for admin buttons
    
    * add initial functionality for admin buttons
    
    Co-authored-by: Sean Murphy <mail.seanmurphy@gmail.com>

--- static/js/base.js
+++ static/js/base.js
@@ -7,3 +7,83 @@ function navBarMenu() {
         x.className = "topnav";
     }
 }
+
+// Admin
+
+const dayPlusOne = document.getElementByID('admin-add-day');
+dayPlusOne.addEventListener('click', event => {
+    addDay();
+});
+
+const initiateSecretSanta = document.getElementByID('admin-secret-santa');
+initiateSecretSanta.addEventListener('click', event => {
+    secretSantaDraw();
+});
+
+const resetTimer = document.getElementByID('admin-reset');
+resetTimer.addEventListener('click', event => {
+    resetDay();
+});
+
+function addDay() {
+    fetch(`/draw/increase_day/`, {
+            method: 'POST',
+            headers: new Headers({
+                'X-CSRFToken': csrftoken,
+            }),
+            credentials: 'same-origin',
+        })
+        .then(res => res.json())
+        .then(data => {
+            UIkit.notification({
+                message: `Day +1 actioned`,
+                status: 'primary',
+                pos: 'top-right',
+                timeout: 5000
+            });
+
+        })
+        .catch(err => console.log(err));
+};
+
+function secretSantaDraw() {
+    fetch(`/draw/increase_day/`, {
+            method: 'POST',
+            headers: new Headers({
+                'X-CSRFToken': csrftoken,
+            }),
+            credentials: 'same-origin',
+        })
+        .then(res => res.json())
+        .then(data => {
+            UIkit.notification({
+                message: `Day set to 11 and Secret Santa drawn`,
+                status: 'primary',
+                pos: 'top-right',
+                timeout: 5000
+            });
+
+        })
+        .catch(err => console.log(err));
+};
+
+function resetDay() {
+    fetch(`/draw/increase_day/`, {
+            method: 'POST',
+            headers: new Headers({
+                'X-CSRFToken': csrftoken,
+            }),
+            credentials: 'same-origin',
+        })
+        .then(res => res.json())
+        .then(data => {
+            UIkit.notification({
+                message: `Day set to 1, Secret Santa draw reset`,
+                status: 'primary',
+                pos: 'top-right',
+                timeout: 5000
+            });
+
+        })
+        .catch(err => console.log(err));
+};
